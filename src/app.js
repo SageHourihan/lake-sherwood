6311028b1e161b07e21dd6001ac398dfcf70ca05
Sage Hourihan <samiho97@gmail.com>
2022-06-22 18:32:59 -0400

    added authentication

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { auth, requiresAuth } = require('express-openid-connect');
+
 require('dotenv').config()
 
 console.log(process.env);
@@ -21,6 +23,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+// AUTH
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: '2709641849f611c7b0ce99dce50b98d43842a7307d50afe05a1edc66b2f07975',
+  baseURL: 'http://localhost:3000',
+  clientID: '6hZk70Nnk8r8OnSshgTyh8WzZaVKVSyC',
+  issuerBaseURL: 'https://dev-dzv70nbk.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,7 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // TODO: add authentication
-app.use('/boat_ramp', boatRampRouter)
+app.use('/boat_ramp', requiresAuth(), boatRampRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
