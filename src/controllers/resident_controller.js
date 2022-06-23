4c3aff8e4f38f0f7ebc38ebc747219e9da8932b2
Sage Hourihan <samiho97@gmail.com>
2022-06-23 13:20:03 -0400

    added specific names to files

--- src/controllers/resident_controller.js
+++ src/controllers/resident_controller.js
@@ -2,7 +2,7 @@ var residentModel = require('../models/residents')
 module.exports = {
     fetchData: function (req, res) {
         residentModel.fetchData(function (data) {
-            res.render('table', { data: data })
+            res.render('boat_ramp_table', { data: data })
             // console.log(data)
         })
     },
